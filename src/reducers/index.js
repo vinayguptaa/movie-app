793bf416f7cfda9eef908f8376fd9431dc6485b2
Vinay Gupta <vinaygupta@Vinays-Mac.local>
2021-10-08 11:16:52 +0530

    add UI actions and reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+export default function movies(state=[], action) { //by def state -> an emoty array
+    if(action.type == 'ADD_MOVIES') {
+        return action.movies;
+    }
+
+    return state;
+}
