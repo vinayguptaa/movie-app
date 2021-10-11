046d628c868b9471312acac401eaa5f3195033b4
Vinay Gupta <vinaygupta@Vinays-Mac.local>
2021-10-11 12:55:44 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const logger = function({dispatch, getState}) {
+  return function(next) {
+    return function(action) {
+      //middleware code
+      console.log('ACTION TYPE: ',action.type);
+      next(action);
+    }
+  }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('creating store: ', store);
 // console.log('state: ', store.getState());
 
